7562d04676d6469836110c1a18d5e6f52a524b79
Nazareno Napoli <nazareno.napoli@davinci.edu.ar>
2025-05-11 14:39:29 -0300

    primera tabla

---
+++ database/migrations/2025_05_11_171534_create_medicos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50);
+            $table->string('apellido', 50);
+            $table->string('especialidad', 50);
+            $table->string('telefono', 15);
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicos');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,14 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::get('prueba', function () {
+    return view('prueba');
+})->name('prueba');
+
+Route::get('prueba2', function () {
+    return view('prueba2');
+})->name('prueba2');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
